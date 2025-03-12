fd6a9779c7ddd6226198bd27b0c785db097817b9
Abdur Razzaque <razzaque.0011@gamail.com>
2025-03-12 22:05:18 +0000

    dynamicaly thamble and title add

--- ph.js
+++ ph.js
@@ -1,3 +1,4 @@
+// button fatch section
 function category() {
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
       .then((res)=>res.json())
@@ -8,7 +9,7 @@ function category() {
 
  
 // {category_id: '1001', category: 'Music'}
-
+// button display section
 const displayCategory = (categories) => {
     let btnShow_container = document.getElementById('display_containner');
     for (const cat of categories) {
@@ -19,5 +20,36 @@ const displayCategory = (categories) => {
     }
 };
 
- 
+// thambale section
+
+function thambale() {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then((res) => res.json())
+    .then((data) => displayVideo(data.videos))
+}
+const displayVideo = (videos) => {
+    let videoContainer = document.getElementById('vcontainer');
+    videos.forEach(video => {
+        let videoElement = document.createElement('div');
+        videoElement.innerHTML = `
+            <div class="card bg-base-100 w-96 shadow-sm">
+                <figure>
+                    <img src="${video.thumbnail}" alt="${video.title}" class="w-full h-48 object-cover rounded-t-lg" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">${video.title}</h2>
+                    <p>A card component has a figure, a body part, and inside the body, there are title and actions parts.</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        `;
+        videoContainer.appendChild(videoElement);
+    });
+};
+
+
+
 category();
+thambale();
